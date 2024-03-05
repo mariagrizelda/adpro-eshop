1f5d09c0a20d49b477e70df2f6a7ce00b75671f1
mariagrizelda <mariahelga423@gmail.com>
2024-03-05 14:03:27 +0700

    [REFACTOR] Add OrderStatus enum

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,9 +1,11 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Getter
@@ -13,21 +15,43 @@ public class Order {
     List<Product> products;
     Long orderTime;
     String author;
-    @Setter
+
     String status;
 
     // Original constructor without status
     public Order(String id, List<Product> products, Long orderTime, String author) {
-        // Your implementation here
-    }
-
-    // New constructor with status
-    @Builder
-    public Order(String id, List<Product> products, Long orderTime, String author, String status) {
         this.id = id;
-        this.products = products;
         this.orderTime = orderTime;
         this.author = author;
-        this.status = status;
+        this.status = "WAITING_PAYMENT";
+
+        if (products.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            this.products = products;
+        }
+    }
+    public Order(String id, List<Product> products, Long orderTime, String author, String status) {
+        this(id, products, orderTime, author); // Call the existing constructor
+
+        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
+
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
+    }
+
+    public void setStatus(String status){
+        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
     }
 }
+
+
+
--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -43,7 +43,7 @@ class OrderTest {
     @Test
     void testCreateOrderSuccessStatus() {
         Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
-                this.products, 1708560000L, "Safira Sudrajat");
+                this.products, 1708560000L, "Safira Sudrajat", "SUCCESS");
         assertEquals("SUCCESS", order.getStatus());
     }
 
@@ -70,3 +70,4 @@ class OrderTest {
         assertThrows(IllegalArgumentException.class, () -> order.setStatus("MEOW"));
     }
 }
+
