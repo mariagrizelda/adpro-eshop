44a05e1c95990e5ce4bc1b92997754a8c0311122
mariagrizelda <mariahelga423@gmail.com>
2024-03-12 01:53:54 +0700

    Implement payment model

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,56 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import java.util.Map;
+
+public class Payment {
+    private String id;
+    private String method;
+    private String status;
+    private Map<String, String> paymentData;
+
+
+    public Payment() {
+
+    }
+
+    public Payment(String id, String method, Map<String, String> paymentData, String status) {
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.status = status;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Map<String, String> getPaymentData() {
+        return paymentData;
+    }
+
+    public void setPaymentData(Map<String, String> paymentData) {
+        this.paymentData = paymentData;
+    }
+
+}
