8d53050ef30d629d4992750447c830e85a5a86f1
mariagrizelda <mariahelga423@gmail.com>
2024-03-12 18:31:55 +0700

    [REFACTOR] Add OrderStatus enum

---
+++ src/main/java/enum/PaymentStatus.java
@@ -0,0 +1,26 @@
+package enums;
+
+import lombok.Getter;
+
+@Getter
+public enum PaymentStatus {
+    WAITING_PAYMENT("WAITING_PAYMENT"),
+    REJECTED("REJECTED"),
+    SUCCESS("SUCCESS"),
+    PENDING("PENDING");
+
+    private final String value;
+
+    private PaymentStatus(String value) {
+        this.value = value;
+    }
+
+    public static boolean contains(String param) {
+        for (PaymentStatus paymentStatus : PaymentStatus.values()) {
+            if (paymentStatus.name().equals(param)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
